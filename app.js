ab5bfde0cefb5af427ca5d4729b283403409f86e
Dag Gebreselasse <dgebreselasse@gmail.com>
2021-03-12 16:15:57 -0600

    added getAuth function that retrieves spotify api token using client ID and secret. function then imports spotify todays top hits playlist using a playlist api endpoint and playlist ID.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
+const qs = require("qs");
+
+
+require('dotenv').config()
+
+
+
 
 const app = express();
 app.use(cors({ origin: "*" }));
@@ -12,7 +20,52 @@ const API_PORT = 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+const getAuth = async () => {
+    const clientID = process.env.clientID;
+    const clientSecret = process.env.clientSecret;
+  
+    const headers = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
+      auth: {
+        username: clientID,
+        password: clientSecret
+      }
+    };
+    const data = {
+      grant_type: "client_credentials"
+    };
+  
+    try {
+      const response = await axios
+        .post(
+          "https://accounts.spotify.com/api/token",
+          qs.stringify(data),
+          headers
+        )
+        .then(res => {
+          console.log(res.data.access_token);
+          axios
+            .get("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
+              headers: {
+                Authorization: "Bearer " + res.data.access_token
+              }
+            })
+            .then(res => {
+            console.log(res.data.tracks.items[1].track)
+          });
+        });
+  
+      //console.log(response.data.access_token);
+      //return response.data.access_token;
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+  getAuth();
 
 
 // launch our backend into a port
