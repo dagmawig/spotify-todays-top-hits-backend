dfc06c7178932a52e6b08819ebf8e75edcc3b279
Dag Gebreselasse <dgebreselasse@gmail.com>
2021-03-12 15:16:29 -0600

    added cors and body-parser module.

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(cors({ origin: "*" }));
+
 const API_PORT = 3001;
 
+
+// bodyParser, parses the request body to be a readable json format
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 
 
