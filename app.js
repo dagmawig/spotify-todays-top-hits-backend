8b94bef95ef7947c3e89516fdf269a9005374483
Dag Gebreselasse <dgebreselasse@gmail.com>
2021-03-20 13:21:27 -0500

    cleaned code and added comments.

--- app.js
+++ app.js
@@ -4,9 +4,7 @@ const cors = require("cors");
 const axios = require("axios");
 const qs = require("qs");
 
-
 require('dotenv').config()
-
 
 const app = express();
 app.use(cors({ origin: "*" }));
@@ -19,49 +17,53 @@ const router = express.Router();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+// function use to get spotify token
 const getToken = async () => {
-    const clientID = process.env.clientID;
-    const clientSecret = process.env.clientSecret;
-  
-    const headers = {
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/x-www-form-urlencoded"
-      },
-      auth: {
-        username: clientID,
-        password: clientSecret
-      }
-    };
-    const data = {
-      grant_type: "client_credentials"
-    };
-  
-      const response = await axios
-        .post(
-          "https://accounts.spotify.com/api/token",
-          qs.stringify(data),
-          headers
-        )
-        let token = response.data.access_token
-        console.log(token)
-        return token;
+  const clientID = process.env.clientID;
+  const clientSecret = process.env.clientSecret;
+
+  const headers = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    auth: {
+      username: clientID,
+      password: clientSecret
+    }
+  };
+  const data = {
+    grant_type: "client_credentials"
   };
 
+  const response = await axios
+    .post(
+      "https://accounts.spotify.com/api/token",
+      qs.stringify(data),
+      headers
+    )
+  let token = response.data.access_token
+  console.log(token)
+  return token;
+};
+
+// router used to get spotify today's top hits playlist.
 router.get("/todaysTop", (req, resp) => {
   getToken()
     .then(res => {
       axios.get("https://api.spotify.com/v1/playlists/37i9dQZF1DXcBWIGoYBM5M", {
-              headers: {
-                Authorization: "Bearer " + res
-              }
-            }).then (data => {
-              return resp.json({success: true, data: data.data});
-            })
-      
+        headers: {
+          Authorization: "Bearer " + res
+        }
+      }).then(data => {
+        return resp.json({ success: true, data: data.data });
+      })
+
     });
 })
 
+// router used to get song features for a given list of track IDs.
 router.post("/trackFeature", (req, resp) => {
   console.log("it gets to feature router");
   const { trackID } = req.body;
@@ -74,14 +76,12 @@ router.post("/trackFeature", (req, resp) => {
           Authorization: "Bearer " + res
         }
       }).then(data => {
-        return resp.json({success: true, data: data.data});
+        return resp.json({ success: true, data: data.data });
       }).catch((e) => {
         console.log(e);
       })
     })
 })
-
-
 
 // append /api for our http requests
 app.use("/", router);
